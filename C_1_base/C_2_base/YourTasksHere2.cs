8a4ba783d199976e8d1f103b4e282f36f71da506
MrDoctor-666 <sheron.dragnil@mai.ru>
2021-03-20 22:13:55 +0300

    AAAA

--- C_1_base/C_2_base/YourTasksHere2.cs
+++ C_1_base/C_2_base/YourTasksHere2.cs
@@ -5,7 +5,85 @@
     // Через это имя должно быть возможно получить доступ к площади и периметру фигур
     // Все числа пусть имееют тип double
     
+    public interface Figure
+    {
+        double Square { get; }
+        double Perim { get; }
+    }
 
+    public class Circle : Figure
+    {
+        private double radius;
+        Circle(double r)
+        {
+            if (r > 0)
+            {
+                this.radius = r;
+            }
+            else throw new System.Exception("this is not a circle");
+        }
+        public double Square
+        {
+            get { return System.Math.PI * System.Math.Pow(radius, 2); }
+        }
+        public double Perim
+        {
+            get { return 2 * System.Math.PI * radius; }
+        }
+    }
 
+    public class Triangle : Figure
+    {
+        //three sides
+        private double A;
+        private double B;
+        private double C;
+        Triangle(double a, double b, double c)
+        {
+            if ((a <= b + c) && (b <= a + c) && (c <= a + b))
+            {
+                this.A = a;
+                this.B = b;
+                this.C = c;
+            }
+            else throw new System.Exception("this is not a triangle");
+        }
+        public double Square
+        {
+            get
+            {
+                double p = this.Perim / 2;
+                return System.Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            }
+        }
+        public double Perim
+        {
+            get { return (A + B + C); }
+        }
+    }
 
+    public class Rectangle : Figure
+    {
+        private double width;
+        private double height;
+
+        Rectangle(double width, double height)
+        {
+            if (width > 0 && height > 0)
+            {
+                this.width = width;
+                this.height = height;
+            }
+            else throw new System.Exception("this is not a rectangle");
+        }
+
+        public double Square
+        {
+            get { return width * height; }
+        }
+        public double Perim
+        {
+            get { return 2 * (width + height); }
+        }
+    }
 }
--- C_1_base/C_3_base/YourTasksHere3.cs
+++ C_1_base/C_3_base/YourTasksHere3.cs
@@ -12,13 +12,70 @@ namespace C_3_base
         // На вход подается массив математических полей, необходимо вывести их сумму, ответ представить в самом оптимальном типе (в самом низком)
         void Sum(Math_Field[] fields)
         {
-            throw new NotImplementedException();
+            Math_Field sum = new N();
+            foreach (Math_Field elem in fields)
+            {
+                sum.ADD(elem);
+            }
+
+            while (sum.isDown) {
+                sum.Dawn();
+            }
+
+            Console.WriteLine(sum);
         }
 
         // Необходимо по массиву строк построить массив соотвествующих математических объкетов
-        Math_Field Reg(string[] fields)
+        Math_Field[] Reg(string[] fields)
         {
-            throw new NotImplementedException();
+            Math_Field[] math_Fields = new Math_Field[fields.Length];
+            //да я че знаю что ли как эти конструкторы работают
+            for (int i = 0; i < fields.Length; i++)
+            {
+                List<string> first = new List<string>();
+                List<string> second = new List<string>();
+                if (fields[i].Contains("i"))
+                {
+                    //пусть С короче выглядит так потому что хз как оно там выглядит "1231+1231i"
+                    first[0] = fields[i].Substring(0, fields[i].IndexOf("+"));
+                    first[1] = fields[i].Substring(fields[i].IndexOf("+") + 1, fields[i].IndexOf("i"));
+                    Q q1, q2;
+
+                    if (first[0].Contains("/"))
+                    {
+                        first[0] = first[0].Substring(0, first[0].IndexOf("/"));
+                        second[0] = first[0].Substring(first[0].IndexOf("/") + 1);
+                        q1 = new Q(first, second);
+                    }
+                    else q1 = new Q(first, new Z(1));
+
+                    first[0] = first[1];
+                    if (first[1].Contains("/"))
+                    {
+                        first[0] = first[1].Substring(0, first[1].IndexOf("/"));
+                        second[0] = first[1].Substring(first[1].IndexOf("/") + 1);
+                        q2 = new Q(first, second);
+                    }
+                    else q2 = new Q(first, new Z(1));
+
+
+                    math_Fields[i] = new C(q1, q2);
+                }
+                else if (fields[i].Contains("/"))
+                {
+                    //пусть "12321/123213"
+                    first[0] = first[1].Substring(0, first[1].IndexOf("/"));
+                    second[0] = first[1].Substring(first[1].IndexOf("/") + 1);
+                    math_Fields[i] = new Q(first, second);
+                }
+                else if (fields[i].Contains("-")) {
+                    first[0] = fields[i];
+                    math_Fields[i] = new Z(first);
+                }
+                else math_Fields[i] = new N(UInt32.Parse(fields[i]));
+            }
+
+            return math_Fields;
         }
     }
 }
