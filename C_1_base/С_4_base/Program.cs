e8d495a5ec53288471c8f3b0b6b50af9714ef449
MrDoctor-666 <sheron.dragnil@mai.ru>
2021-04-08 23:17:55 +0300

    4 dz

---
+++ C_1_base/С_4_base/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace С_4_base
+{
+    class YourTaskHere
+    {
+        // Используя средства рефлексии и консольного вывода, вывидете все имена (идентификаторы) типов данных из сборки C_2_base
+        static void Main(string[] args)
+        {
+            Assembly assembly;
+            assembly = Assembly.LoadFrom("C_2_base.dll");
+            Type[] types = assembly.GetTypes();
+            foreach (Type type in types)
+                Console.WriteLine(type.Name);
+            Console.ReadLine();
+        }
+    }
+}
