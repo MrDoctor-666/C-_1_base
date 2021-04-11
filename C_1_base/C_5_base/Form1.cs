63ca087de2d938b0863bf665989c8c5823d88c33
MrDoctor-666 <sheron.dragnil@mai.ru>
2021-04-11 22:27:02 +0300

    5

---
+++ C_1_base/C_5_base/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace C_5_base
+{
+    public partial class Form1 : Form
+    {
+        string dir;
+        string[] dirs, files;
+
+        private void DisplayDir()
+        {
+            listView1.Clear();
+            dirs = Directory.GetDirectories(dir);
+            files = Directory.GetFiles(dir);
+            foreach (string dirr in dirs)
+            {
+                ListViewItem lvi = new ListViewItem();
+                // установка названия файла
+                lvi.Text = dirr.Remove(0, dirr.LastIndexOf('\\') + 1);
+                lvi.BackColor = Color.PaleVioletRed;
+                listView1.Items.Add(lvi);
+            }
+            foreach (string file in files)
+            {
+                ListViewItem lvi = new ListViewItem();
+                // установка названия файла
+                lvi.Text = file.Remove(0, file.LastIndexOf('\\') + 1);
+                listView1.Items.Add(lvi);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            dir = "C:\\";
+            DisplayDir();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dir != "C:\\"){
+                dir = dir.Remove(dir.Length - 1);
+                dir = dir.Remove(dir.LastIndexOf('\\') + 1);
+                DisplayDir();
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!listView1.SelectedItems[0].Text.Contains("."))
+            {
+                dir += listView1.SelectedItems[0].Text + '\\';
+                DisplayDir();
+            }
+            else
+            {
+                //open file
+                openFileDialog1.InitialDirectory = dir;
+                openFileDialog1.FileName = listView1.SelectedItems[0].Text;
+                openFileDialog1.ShowDialog();
+                DisplayDir();
+            }
+        }
+    }
+}
---
+++ C_1_base/С_6_ base/YourTaskHere.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С_6__base
+{
+    class YourTaskHere
+    {
+        //1 - Реализуйте метод LINQer, который на основе входной последовательности формирует LINQ-запрос, возвращающий элементы последовательности, котороые больше среднего значения входной последовательности 
+        IEnumerator<T> LINQer<T>(T[] array)
+        {
+            var mid = array.Average();
+
+            IEnumerator < T > newArr = from elem in array
+                                       where elem > mid
+                                       select elem;
+            return newArr;
+        
+        }
+
+        //2 - Реализуйте метод, который будет асинхронно искать и выводить в консоль факториал чисел от N до 1
+        //3 - в проекте "Test" создайте класс для тестирования обоих задач
+    }
+}
